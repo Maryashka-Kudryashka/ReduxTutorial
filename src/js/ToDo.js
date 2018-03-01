9cfa4566b642e624b90e473bb6ece8bfb6ca5182
maryashka-kudryashka <Maryashka3@gmail.com>
2018-03-01 08:57:19 +0200

    Todo - Adding a Todo

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -2,7 +2,8 @@ import {createStore} from "redux"
 import expect from "expect"
 import React from "react"
 import ReactDOM from "react-dom"
-// import { combineReducers } from "redux"
+import { combineReducers } from "redux"
+import { component } from "react";
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -52,21 +53,6 @@ const visibilityFilter = (
   }
 }
 
-const combineReducers = (reducers) => {
-  return (state = {}, action) => {
-    return Object.keys(reducers).reduce (
-      (nextState, key) => {
-        nextState[key] = reducers[key] (
-          state[key],
-          action
-        );
-        return nextState;
-      },
-      {}
-    );
-  };
-};
-
 const todoApp = combineReducers({
   todos: todos,
   visibilityFilter: visibilityFilter
@@ -74,48 +60,44 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('---------------');
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+        }}>
+        Add todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+            )}
+          </ul>
+        </div>
+    );
+  }
+}
 
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux'
-});
+const render = () => {
+  ReactDOM.render (
+    <TodoApp
+    todos={store.getState().todos}
+    />,
+    document.getElementById('root')
+   )
+}
 
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go shopping'
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching TOGGLE_TODO');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED'
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
+store.subscribe(render);
+render();
