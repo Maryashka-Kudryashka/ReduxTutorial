2a464cde2a32d10585844c39fbfa5c64b6efd7b7
maryashka-kudryashka <Maryashka3@gmail.com>
2018-03-01 09:46:49 +0200

    Extracting Presentatonal Components

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -83,6 +83,38 @@ const FilterLink = ({
   )
 }
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+    <li
+    onClick={onClick}
+    style={{
+      textDecoration:
+        completed ?
+        'line-through' :
+        'none'
+    }}>
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+  {todos.map(todo =>
+    <Todo
+      key={todo.id}
+      {...todo}
+      onClick={() => onTodoClick(todo.id)}
+    />
+  )}
+  </ul>
+);
+
 const getVisibleTodos = (
   todos,
   filter
@@ -128,25 +160,14 @@ class TodoApp extends React.Component {
         }}>
         Add todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{
-                textDecoration:
-                  todo.completed ?
-                  'line-through' :
-                  'none'
-              }}>
-              {todo.text}
-            </li>
-            )}
-          </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          } />
           <p>
             Show:
             {' '}
