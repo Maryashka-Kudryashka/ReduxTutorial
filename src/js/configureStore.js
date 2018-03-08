7e9031aff145b4612759a11504d0b9275d45a998
maryashka-kudryashka <Maryashka3@gmail.com>
2018-03-08 06:20:51 +0200

    Adding a Fake Backend to the Project

--- src/js/configureStore.js
+++ src/js/configureStore.js
@@ -1,7 +1,5 @@
 import todoApp from "./reducers";
 import {createStore} from "redux";
-import { loadState, saveState } from "./localStorage"
-import throttle from "lodash/throttle";
 
 const addLoggingToDispatch = (store) => {
   const rawDispatch = store.dispatch;
@@ -20,21 +18,13 @@ const addLoggingToDispatch = (store) => {
 }
 
 const configureStore = () => {
-  const persistedState = loadState();
   const store = createStore(
-    todoApp,
-    persistedState
+    todoApp
   );
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store);
   }
-
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos
-    });
-  }, 1000));
   return store;
-};
+}
 
 export default configureStore;
